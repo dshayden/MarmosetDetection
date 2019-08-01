29a9b4b194957595760595cabaa31c04ef1ad04e
David Hayden <dshayden@mit.edu>
2019-08-01 13:23:30 -0400

    Minor fix

--- scripts/mrcnn_marmoset.py
+++ scripts/mrcnn_marmoset.py
@@ -36,20 +36,20 @@ import skimage.draw
 
 import du, IPython as ip
 
-# Root directory of the project
-ROOT_DIR = os.path.abspath("../../")
-
-# Import Mask RCNN
-sys.path.append(ROOT_DIR)  # To find local version of the library
-from mrcnn.config import Config
-from mrcnn import model as modellib, utils
-
-# Path to trained weights file
-COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
-
-# Directory to save logs and model checkpoints, if not provided
-# through the command line argument --logs
-DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+# # Root directory of the project
+# ROOT_DIR = os.path.abspath("../../")
+#
+# # Import Mask RCNN
+# sys.path.append(ROOT_DIR)  # To find local version of the library
+# from mrcnn.config import Config
+# from mrcnn import model as modellib, utils
+#
+# # Path to trained weights file
+# COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+#
+# # Directory to save logs and model checkpoints, if not provided
+# # through the command line argument --logs
+# DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 
 ############################################################
 #  Configurations
